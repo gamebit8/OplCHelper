01cd50fc352de42992459ce69ccb7598b7e1dc63
Gamebit8 <gamebit8@gmail.com>
2024-07-17 10:37:39 +0400

    Add AssemblyInfo.cs

---
+++ src/UI/ConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("OplCHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OplCHelper")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
